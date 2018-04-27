3f1208ebc396399d39ab1a6927d0487c933207f9
Jon Campbell <jonathan.campbell@firsthelpline.com>
2018-04-27 18:31:11 +0100

    Add program that reports if two objects of the same type are equal

---
+++ FUnit.Cli/Program.fs
@@ -0,0 +1,33 @@
+﻿type Result = PASS | FAIL | EXCEPTION
+
+let Is predicate expected actual =
+    try
+        match predicate expected actual with
+        | true  -> PASS
+        | false -> FAIL
+    with
+    | Failure _ -> EXCEPTION
+
+let ToResultString = function
+    | PASS  -> "PASS"
+    | FAIL -> "FAIL"
+    | EXCEPTION -> "UNHANDLED EXCEPTION"
+
+type IReportResults =
+    abstract member Report: string -> unit
+
+type ConsoleReporter() =
+
+    interface IReportResults with
+        member this.Report result = printfn "%s" result
+
+let ReportResults (reporter:IReportResults) =
+    Is (fun expected actual -> expected.Equals actual) 10 10
+    |> ToResultString
+    |> reporter.Report
+
+[<EntryPoint>]
+let main argv =
+    new ConsoleReporter()
+    |> ReportResults 
+    0
