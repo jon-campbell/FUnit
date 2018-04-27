64321d7ec4f2bd816308dff053be7417ae98822b
Jon Campbell <jonathan.campbell@firsthelpline.com>
2018-04-27 18:48:00 +0100

    **WIP issue with partially applied function type hardening on first use

--- FUnit.Cli/Program.fs
+++ FUnit.Cli/Program.fs
@@ -21,13 +21,15 @@ type ConsoleReporter() =
     interface IReportResults with
         member this.Report result = printfn "%s" result
 
-let ReportResults (reporter:IReportResults) =
-    Is (fun expected actual -> expected.Equals actual) 10 10
+let ReportResults (reporter:IReportResults) expected actual =
+    Is (fun expected actual -> expected.Equals actual) expected actual
     |> ToResultString
     |> reporter.Report
 
 [<EntryPoint>]
 let main argv =
-    new ConsoleReporter()
-    |> ReportResults 
+    let areEqual = new ConsoleReporter() |> ReportResults
+    areEqual 10 10
+    areEqual true true
+    areEqual "some" "fail"
     0
